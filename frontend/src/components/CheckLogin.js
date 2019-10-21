2cdc9c55700fd21f5e6959fd926221063cba5c78
ZoeLiao <zoejoyuliao@gmail.com>
2019-10-21 09:06:15 +0800

    Improve CheckLogin

--- frontend/src/components/CheckLogin.js
+++ frontend/src/components/CheckLogin.js
@@ -5,15 +5,17 @@ import {withRouter} from 'react-router-dom'
 @withRouter
 class CheckLogin extends Component {
     componentDidMount() {
-        axios.get('/api/words/')
-            .then(res => {
-                if(res.status === 200) {
-                    if(res.data.code === 0) {
-                    }else {
-                        this.props.history.push('/user/login/')
+        if(window.location.pathname.startsWith('/api/')) {
+            axios.get('/api/words/')
+                .then(res => {
+                    if(res.status === 200) {
+                        if(res.data.code === 0) {
+                        }else {
+                            this.props.history.push('/login/')
+                        }
                     }
-                }
-            })
+                })
+        }
     }
     render() {
         return null;
